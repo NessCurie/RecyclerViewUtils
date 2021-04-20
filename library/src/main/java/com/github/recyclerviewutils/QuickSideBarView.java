0d53c21772885a364f9bce623474a08579d9be17
nesscurie <nesscurie@live.com>
2021-04-20 19:12:11 +0800

    QuickSideBarView add horizontal mode

--- library/src/main/java/com/github/recyclerviewutils/QuickSideBarView.java
+++ library/src/main/java/com/github/recyclerviewutils/QuickSideBarView.java
@@ -10,27 +10,49 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 /**
  * 置于页面end并设置marginEnd之后触摸位置会向左偏移,只能不设置marginEnd拓宽触摸范围
  */
 public class QuickSideBarView extends View {
 
+    public static final int HORIZONTAL = 0;
+    public static final int VERTICAL = 1;
+
+    public static final int CENTER = 0;
+    public static final int TOP = 1;
+    public static final int BOTTOM = 2;
+    public static final int START = 3;
+    public static final int END = 4;
+
     public interface OnQuickSideBarTouchListener {
-        void onLetterStateChanged(boolean needShowTips, String letter, int position, float y);
+        /**
+         * @param needShowTips 是否需要显示tipsView
+         * @param rowMargin    横向时是 x  竖向时是y
+         */
+        void onLetterStateChanged(boolean needShowTips, String letter, int position, float rowMargin);
     }
 
-    private String[] letters;
+    private ArrayList<String> letters = new ArrayList<>();
+    private ArrayList<String> hasLetters = new ArrayList<>();
     private int choose = -1;
-    private float textSize = 20;
-    private float textSizeChoose = 15;
+    private float textSize = 18;
+    private float textSizeChoose = 22;
+    private float textSizeHas = 22;
     private int textColor;
     private int textColorChoose;
+    private int textColorHas;
     private float letterMargin;
     private Paint paint = new Paint();
     private Rect rect = new Rect();
     private OnQuickSideBarTouchListener listener;
     private float maxTextCenterX;
+    private float maxTextCenterY;
+    private int orientation = VERTICAL;
+    private int gravity = CENTER;
 
     public QuickSideBarView(Context context) {
         this(context, null);
@@ -46,112 +68,212 @@ public class QuickSideBarView extends View {
     }
 
     private void init(Context context, AttributeSet attrs) {
-        letters = context.getResources().getStringArray(com.github.recyclerviewutils.R.array.quickSideBarLetters);
+        letters.addAll(Arrays.asList(context.getResources().getStringArray(R.array.quickSideBarLetters)));
         textColor = Color.BLACK;
         textColorChoose = Color.BLACK;
+        textColorHas = Color.BLACK;
         if (attrs != null) {
-            TypedArray a = getContext().obtainStyledAttributes(attrs, com.github.recyclerviewutils.R.styleable.QuickSideBarView);
-            textColor = a.getColor(com.github.recyclerviewutils.R.styleable.QuickSideBarView_sidebarTextColor, textColor);
-            textColorChoose = a.getColor(com.github.recyclerviewutils.R.styleable.QuickSideBarView_sidebarChooseTextColor, textColorChoose);
-            textSize = a.getDimension(com.github.recyclerviewutils.R.styleable.QuickSideBarView_sidebarTextSize, textSize);
-            textSizeChoose = a.getDimension(com.github.recyclerviewutils.R.styleable.QuickSideBarView_sidebarChooseTextSize, textSizeChoose);
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.QuickSideBarView);
+            textColor = a.getColor(R.styleable.QuickSideBarView_sidebarTextColor, textColor);
+            textColorChoose = a.getColor(R.styleable.QuickSideBarView_sidebarChooseTextColor, textColorChoose);
+            textColorHas = a.getColor(R.styleable.QuickSideBarView_sidebarHasTextColor, textColorHas);
+            textSize = a.getDimension(R.styleable.QuickSideBarView_sidebarTextSize, textSize);
+            textSizeChoose = a.getDimension(R.styleable.QuickSideBarView_sidebarChooseTextSize, textSizeChoose);
+            textSizeHas = a.getDimension(R.styleable.QuickSideBarView_sidebarHasTextSize, textSizeHas);
+            orientation = a.getInt(R.styleable.QuickSideBarView_sidebarOrientation, VERTICAL);
+            gravity = a.getInt(R.styleable.QuickSideBarView_sidebarTextGravity, CENTER);
             a.recycle();
+        }
+    }
+
+    public void setOrientation(int orientation) {
+        if (this.orientation != orientation) {
+            this.orientation = orientation;
+            requestLayout();
+        }
+    }
+
+    public void setGravity(int gravity) {
+        if (this.gravity != gravity) {
+            if (gravity == START || gravity == END) {
+                if (orientation == VERTICAL) {
+                    this.gravity = gravity;
+                    invalidate();
+                }
+            } else if (gravity == TOP || gravity == BOTTOM) {
+                if (orientation == HORIZONTAL) {
+                    this.gravity = gravity;
+                    invalidate();
+                }
+            } else {
+                this.gravity = gravity;
+                invalidate();
+            }
         }
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        paint.setTextSize(textSize);
         paint.setAntiAlias(true);
-        paint.setColor(textColor);
+
+        paint.setTextSize(Math.max(textSizeChoose, Math.max(textSizeHas, textSize)));
+
         int lettersHeight = 0;
+        float rectMaxHeight = 0f;
         float rectMaxWith = 0f;
         for (String s : letters) {
             rect.setEmpty();
+
             paint.getTextBounds(s, 0, s.length(), rect);
             if (rectMaxWith < rect.width()) {
                 rectMaxWith = rect.width();
             }
             lettersHeight += rect.height();
+
+            if (rectMaxHeight < rect.height()) {
+                rectMaxHeight = rect.height();
+            }
         }
         maxTextCenterX = rectMaxWith / 2;
-        letterMargin = ((float) (getMeasuredHeight() - lettersHeight)) / letters.length;
+        maxTextCenterY = rectMaxHeight / 2;
+        if (orientation == VERTICAL) {
+            letterMargin = ((float) (getMeasuredHeight() - lettersHeight)) / letters.size();
+        } else {    //横向时每个字母宽绘制一样
+            letterMargin = ((float) getMeasuredWidth() - rectMaxWith * letters.size()) / letters.size();
+        }
     }
 
     protected void onDraw(Canvas canvas) {
-        float currentY = letterMargin / 2;
-        for (int i = 0; i < letters.length; i++) {
-            String s = letters[i];
-            paint.setTextAlign(Paint.Align.CENTER);
-            paint.setAntiAlias(true);
-            if (i == choose) {
-                paint.setColor(textColorChoose);
-                paint.setTextSize(textSizeChoose);
-            } else {
-                paint.setColor(textColor);
-                paint.setTextSize(textSize);
+        float current = letterMargin / 2;
+        if (orientation == VERTICAL) {
+            for (int i = 0; i < letters.size(); i++) {
+                String s = letters.get(i);
+                paint.setTextAlign(Paint.Align.CENTER);
+                paint.setAntiAlias(true);
+                if (i == choose) {
+                    paint.setColor(textColorChoose);
+                    paint.setTextSize(textSizeChoose);
+                } else {
+                    paint.setColor(textColor);
+                    paint.setTextSize(textSize);
+                }
+                rect.setEmpty();
+                paint.getTextBounds(s, 0, s.length(), rect);
+
+                Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+                float distance = (fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom;
+
+                float drawRectY = Math.abs(rect.centerY()) + distance;
+
+                current += drawRectY;
+                canvas.drawText(s, maxTextCenterX, current, paint);
+
+                current += (rect.height() - drawRectY);
+                current += letterMargin;
+                paint.reset();
             }
-            rect.setEmpty();
-            paint.getTextBounds(s, 0, s.length(), rect);
+        } else {
+            for (int i = 0; i < letters.size(); i++) {
+                String s = letters.get(i);
+                paint.setTextAlign(Paint.Align.CENTER);
+                paint.setAntiAlias(true);
+                if (i == choose) {
+                    paint.setColor(textColorChoose);
+                    paint.setTextSize(textSizeChoose);
+                } else if (hasLetters.contains(s)) {
+                    paint.setColor(textColorHas);
+                    paint.setTextSize(textSizeHas);
+                } else {
+                    paint.setColor(textColor);
+                    paint.setTextSize(textSize);
+                }
+                rect.setEmpty();
+                paint.getTextBounds(s, 0, s.length(), rect);
 
-            Paint.FontMetrics fontMetrics = paint.getFontMetrics();
-            float distance = (fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom;
+                Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+                float distance = (fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom;
+                float drawRectY;
+                if (gravity == CENTER) {
+                    drawRectY = getMeasuredHeight() / 2f + distance;
+                } else if (gravity == BOTTOM) {
+                    drawRectY = maxTextCenterY + (getMeasuredHeight() - maxTextCenterY * 2) + distance;
+                } else {
+                    drawRectY = maxTextCenterY + distance;
+                }
 
-            float drawRectY = Math.abs(rect.centerY()) + distance;
+                current += maxTextCenterX;
+                canvas.drawText(s, current, drawRectY, paint);
 
-            currentY += drawRectY;
-            canvas.drawText(s, maxTextCenterX, currentY, paint);
-
-            currentY += (rect.height() - drawRectY);
-            currentY += letterMargin;
-            paint.reset();
+                current += maxTextCenterX;
+                current += letterMargin;
+                paint.reset();
+            }
         }
     }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
         int action = event.getAction();
-        float y = event.getY();
-        float rawY = event.getRawY();
-        float yMargin = rawY - y;
         float current = 0;
         int newChoose = 0;
-        float letterCenterRawY = 0;
+        float letterCenterRawMargin = 0;
 
-        paint.setTextAlign(Paint.Align.CENTER);
-        paint.setAntiAlias(true);
-        paint.setTextSize(textSizeChoose);
-        for (int i = 0; i < letters.length; i++) {
-            String s = letters[i];
-            rect.setEmpty();
-            paint.getTextBounds(s, 0, s.length(), rect);
-            letterCenterRawY = current + letterMargin / 2f + Math.abs(rect.centerY());
-            current += (rect.height() + letterMargin);
-            if (current > y) {
-                newChoose = i;
-                break;
+        if (orientation == VERTICAL) {
+            float y = event.getY();
+            float rawY = event.getRawY();
+            float yMargin = rawY - y;
+
+            paint.setTextAlign(Paint.Align.CENTER);
+            paint.setAntiAlias(true);
+            paint.setTextSize(textSizeChoose);
+            for (int i = 0; i < letters.size(); i++) {
+                String s = letters.get(i);
+                rect.setEmpty();
+                paint.getTextBounds(s, 0, s.length(), rect);
+                letterCenterRawMargin = current + letterMargin / 2f + Math.abs(rect.centerY());
+                current += (rect.height() + letterMargin);
+                if (current > y) {
+                    newChoose = i;
+                    break;
+                }
             }
+            letterCenterRawMargin += yMargin;
+        } else {
+            float x = event.getX();
+            float rawX = event.getRawX();
+            float xMargin = rawX - x;
+
+            for (int i = 0; i < letters.size(); i++) {
+                letterCenterRawMargin = current + letterMargin / 2f + maxTextCenterX;
+                current += letterMargin + maxTextCenterX * 2;
+                if (current > x) {
+                    newChoose = i;
+                    break;
+                }
+            }
+            letterCenterRawMargin += xMargin;
         }
-        letterCenterRawY += yMargin;
 
         switch (action) {
             case MotionEvent.ACTION_DOWN:
                 choose = newChoose;
                 invalidate();
                 if (listener != null) {
-                    listener.onLetterStateChanged(true, letters[newChoose], choose, letterCenterRawY);
+                    listener.onLetterStateChanged(true, letters.get(newChoose), choose, letterCenterRawMargin);
                 }
                 break;
             case MotionEvent.ACTION_MOVE:
                 if (event.getX() < 0 || event.getY() < 0 || event.getX() > getWidth() || event.getY() > getHeight()) {
-                    listener.onLetterStateChanged(false, letters[newChoose], choose, letterCenterRawY);
+                    if (listener != null) {
+                        listener.onLetterStateChanged(false, letters.get(newChoose), choose, letterCenterRawMargin);
+                    }
                 } else {
                     if (choose != newChoose) {
                         choose = newChoose;
                         invalidate();
                         if (listener != null) {
-                            listener.onLetterStateChanged(true, letters[newChoose], choose, letterCenterRawY);
+                            listener.onLetterStateChanged(true, letters.get(newChoose), choose, letterCenterRawMargin);
                         }
                     }
                 }
@@ -159,12 +281,13 @@ public class QuickSideBarView extends View {
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
                 if (listener != null) {
-                    listener.onLetterStateChanged(false, letters[newChoose], choose, letterCenterRawY);
+                    listener.onLetterStateChanged(false, letters.get(newChoose), choose, letterCenterRawMargin);
                 }
                 choose = -1;
                 invalidate();
                 break;
         }
+
         return true;
     }
 
@@ -172,9 +295,20 @@ public class QuickSideBarView extends View {
         this.listener = listener;
     }
 
-    public void setLetters(String[] letters) {
-        this.letters = new String[letters.length];
-        System.arraycopy(letters, 0, this.letters, 0, letters.length);
+    public void setLetters(ArrayList<String> letters) {
+        this.letters.clear();
+        this.letters.addAll(letters);
+        invalidate();
+    }
+
+    public void setHasLetters(ArrayList<String> hasLetters) {
+        this.hasLetters.clear();
+        this.hasLetters.addAll(hasLetters);
+        invalidate();
+    }
+
+    public void setChoose(String choose) {
+        this.choose = letters.indexOf(choose);
         invalidate();
     }
 }
